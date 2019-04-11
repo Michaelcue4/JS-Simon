0b989acf5a7914cd1028127014c3193710abd4a8
Michael Cue <MichaelCue@Michaels-MacBook-Pro-2.local>
2019-04-11 14:36:29 -0400

    fixed id html

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ const GreenChoice  = document.getElementsByClassName("green");
 const BlueChoice = document.getElementsByClassName("blue");
 const RedChoice = document.getElementsByClassName("red")
 const YellowChoice = document.getElementsByClassName("yellow");
-async function(){
+async function gameStart(){
     return promise();
 }

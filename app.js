1e611577ed1890e7437080275162819d7a20caea
Michael Cue <MichaelCue@Michaels-MacBook-Pro-2.local>
2019-04-11 14:17:57 -0400

    start js

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 let ColorWheel = ["red","green","blue","yellow"];
 let CurrentColorWheel = [];
+let CpuColorWheel = [];
+const GreenChoice  = document.getElementsByClassName("green");
+const BlueChoice = document.getElementsByClassName("blue");
+const RedChoice = document.getElementsByClassName("red")
+const YellowChoice = document.getElementsByClassName("yellow");
+async function(){
+    return promise();
+}

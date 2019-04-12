f13a0ed7a04a332b704f61f54802a03246e77a89
Michael Cue <MichaelCue@Michaels-MacBook-Pro-2.local>
2019-04-12 11:08:34 -0400

    adding more css

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 let ColorWheel = ["red","green","blue","yellow"];
-let CurrentColorWheel = [];
+let PlayerColorWheel = [];
 let CpuColorWheel = [];
+let easy = 1000;
+let medium = 1000;
+let hard = 1000;
+let currentdiff = 0;
+let gameOn = fasle;
+
+
 const GreenChoice  = document.getElementsByClassName("green");
 const BlueChoice = document.getElementsByClassName("blue");
 const RedChoice = document.getElementsByClassName("red")
 const YellowChoice = document.getElementsByClassName("yellow");
-async function gameStart(){
-    return promise();
+
+document.getElementById('Easy').onclick = function(){
+currentdiff = easy;
+let currentDiffBack = this;
+this.style.backgroundColor = "yellow";
+document.getElementById("Medium").style.backgroundColor = "black";
+document.getElementById("Hard").style.backgroundColor = "black";
+}
+function Startgame (){
+
 }

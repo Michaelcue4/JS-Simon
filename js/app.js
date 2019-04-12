db63979520bdd724a18c9edc00ee4996ef5e0227
Michael Cue <MichaelCue@Michaels-MacBook-Pro-2.local>
2019-04-12 14:33:34 -0400

    big push

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
-let game = document.querySelector("#");
+//let game = document.querySelector("button > #gameStart");
 // document
-// console.log(game)
+document.querySelector('#gameStart').onclick = () => {
+    console.log("Hello World");
+};
+//console.log(game)
 
 let ColorWheel = ["red","green","blue","yellow"];
 let PlayerColorWheel = [];
@@ -11,6 +14,8 @@ const hard = 1000;
 let currentdiff = 0;
 let gameOn = false;
 let currentDiffBack; 
+let Startcolor;
+let Diffselect = false;
 
 
 const GreenChoice  = document.getElementsByClassName("green");
@@ -18,10 +23,10 @@ const BlueChoice = document.getElementsByClassName("blue");
 const RedChoice = document.getElementsByClassName("red")
 const YellowChoice = document.getElementsByClassName("yellow");
 const easyButton = document.getElementById("easy");
-// const game = document.getElementById("gameStart");
+const game = document.getElementById("gameStart");
+
 console.log(game)
-console.log(GreenChoice)
-// document.getElementById("easy").onClick = function(){
+// document.getElementById("Easy").onClick = function(){
 // currentdiff = easy;
 // let currentDiffBack = this;
 // this.style.backgroundColor = "yellow";
@@ -29,8 +34,15 @@ console.log(GreenChoice)
 // document.getElementById("Hard").style.backgroundColor = "black";
 // }
 
-// Startgame.addEventListener("click",(event)=>{
-//     gameOn = true;
-//     console.log("start");
-//     alert("start");
-// })
+game.addEventListener("click",(event)=>{
+    gameOn = true;
+
+    document.getElementsByClassName("button")[0].className= "startcolor";
+    console.log("start");
+
+})
+easyButton.addEventListener("click",(event)=>{
+    document.getElementsByClassName("button")[0].className= "startcolor";
+    return;
+
+})

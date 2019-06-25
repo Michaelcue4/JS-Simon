cf95ebfb1d29db1db55e06e5f3c0061a7c9bb264
Michaelcue4 <michaelcue@me.com>
2019-06-25 19:06:44 -0400

    version 1

--- js/app.js
+++ js/app.js
@@ -2,12 +2,16 @@ let allMoves = [$('#red'), $('#green'), $('#yellow'), $('#blue')];
 let botMoves = [];
 let score = 0;
 let playerTurn = false;
+let botisRunning = false
 let playerMoves = [];
+$('#win1').hide();
+$('#end1').hide();
 
 function botsTurn(){
     let interval = 2000;
     botMoves.push(allMoves[Math.floor(Math.random() * allMoves.length)]);
     playerMoves = [];
+    botisRunning = true;
     botMoves.forEach(move => {
         botMakesMove(move, interval);
         interval += 2000;
@@ -22,18 +26,22 @@ function botMakesMove(move, interval){
 }
 function wrongMove(){
     // score--;
+    botisRunning = false;
     $('#end1').show();
     $('#gameStart').show();
     document.querySelector("#score > .currentScore").innerText = score;
 
 }
-
+function win(){
+    $('#win1').show();
+    $('#gameStart').show();
+    document.querySelector("#score > .currentScore").innerText = score;
+}
 $('#gameStart').click(e => {
     e.preventDefault();
     botsTurn();
     playerTurn = true;
     $('#gameStart').hide(); 
-    $('#end1').hide();
 });
 allMoves.forEach(move => {
     move.click(() => {
@@ -45,7 +53,11 @@ allMoves.forEach(move => {
  
             if(playerMoves.length === botMoves.length && playerMoves[playerMoves.length - 1] === botMoves[playerMoves.length - 1]){
                 score++;
-                botsTurn();         
+                if(botisRunning == true)
+                {botsTurn();}
+            }
+            if(score == 2){
+                win();
             }
         }
     }); 
